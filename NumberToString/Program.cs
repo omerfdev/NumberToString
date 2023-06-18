7ab52252ac13375ce445c5449f26e76214dbfa7c
omerfdev <omerfdev@gmail.com>
2023-06-18 21:06:24 +0300

    Commit

--- NumberToString/Program.cs
+++ NumberToString/Program.cs
@@ -1,38 +1,50 @@
 ﻿using System.Text;
 
-Console.WriteLine("Please Enter 3 Digit Number");
-int number = Convert.ToInt32(Console.ReadLine());
-string loopnumber = Convert.ToString(number);
-string tostringnumber;
-StringBuilder sb = new StringBuilder();
-int numberonly;
-for (int i = loopnumber.Length - 1; i >= 0; i--)
+public static void Main(string[] args)
 {
 
-
-    if (i == 0)
-    {
-        numberonly = number % 10;
-        string[] unit = { " tl", " one ", " two ", " three ", " four ", " five ", " six ", " seven ", " eigth ", " nine " };
-        tostringnumber = unit[numberonly];
-        sb.Append(tostringnumber);
-    }
-    if (i == 1)
-    {
-        numberonly = (number / 10) % 10;
-        string[] unit = { "", " ten", " twenty", " thirty", " fourthy", " fifthy", " sixty", " seventy", " eigthy", " ninety" };
-        tostringnumber = unit[numberonly];
-        sb.Append(tostringnumber);
-    }
-    if (i == 2)
-    {
-        numberonly = number / 100;
-        string[] unit = { "", "one hundred", "two hundred", "three hundreed", "four hundred", "five hundred", "six hundred", "seven hundred", "eigth hundred", "nine hundred" };
-        tostringnumber = unit[numberonly];
-        sb.Append(tostringnumber);
-    }
-
-
+    numberToStringDelegate;
+    Console.ReadLine();
 }
-Console.WriteLine(sb);
-Console.ReadLine();
+
+public delegate void NumberToStringDelegate();
+public static NumberToStringDelegate numberToStringDelegate = NumberToStringMethod;
+public static void NumberToStringMethod()
+{
+    Console.WriteLine("Please Enter 3 Digit Number");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string loopNumber = Convert.ToString(number);
+    string toStringNumber;
+    StringBuilder sb = new StringBuilder();
+    int numberOnly;
+    for (int i = loopNumber.Length - 1; i >= 0; i--)
+    {
+
+
+        if (i == 0)
+        {
+            numberOnly = number % 10;
+            string[] unit = { " tl", " one ", " two ", " three ", " four ", " five ", " six ", " seven ", " eigth ", " nine " };
+            toStringNumber = unit[numberOnly];
+            sb.Append(toStringNumber);
+        }
+        if (i == 1)
+        {
+            numberOnly = (number / 10) % 10;
+            string[] unit = { "", " ten", " twenty", " thirty", " fourthy", " fifthy", " sixty", " seventy", " eigthy", " ninety" };
+            toStringNumber = unit[numberOnly];
+            sb.Append(toStringNumber);
+        }
+        if (i == 2)
+        {
+            numberOnly = number / 100;
+            string[] unit = { "", "one hundred", "two hundred", "three hundreed", "four hundred", "five hundred", "six hundred", "seven hundred", "eigth hundred", "nine hundred" };
+            toStringNumber = unit[numberOnly];
+            sb.Append(toStringNumber);
+        }
+
+
+    }
+    Console.WriteLine(sb);
+  
+}
